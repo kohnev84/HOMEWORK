5feb500fa7c948ae91bcf268302fbf3dd8b15a43
kohnev84 <kohnev84@gmail.com>
2022-08-04 18:12:56 +0300

    добавил 20 домашнию работу

---
+++ hw_20/index.js
@@ -0,0 +1,32 @@
+// for (let numbers = 1; numbers <= 1000; numbers++) {
+
+//     document.getElementById("container").innerHTML += `${numbers}<br>`
+
+// }
+
+// for (let numbers = 1000; numbers > 0; numbers--) {
+
+//     document.getElementById("container").innerHTML += `${numbers}<br>`
+
+// }
+
+// for(let numbers = 1; numbers <= 1000; numbers = numbers + 7) {
+    
+//      document.getElementById("container").innerHTML += `${numbers}<br>`
+
+// }
+
+// for(let numbers = 1; numbers <= 1000; numbers = numbers + 7) {
+    
+//      document.getElementById("container").innerHTML += `${numbers}<br>`
+
+// }
+
+// for(let numbers = 2; numbers <= 1000; numbers = numbers += 2) {
+//          document.getElementById("container").innerHTML += `${numbers}<br>`     
+// }
+
+// for(let numbers = 1; numbers <= 1000; numbers = numbers += 2) {
+//      document.getElementById("container").innerHTML += `${numbers}<br>`     
+// }
+
