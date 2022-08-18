98e211cf75166756873565c0653de3224ea46e95
kohnev84 <kohnev84@gmail.com>
2022-08-18 17:28:03 +0300

    Добавил 27 домашнию работу

---
+++ hw_27/index.js
@@ -0,0 +1,41 @@
+async function foo() {
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  
+  if(response.ok) {
+    let json = await response.json();
+    console.log(json)
+  }else {
+     alert("Ошибка:" + response.status)
+  }
+}
+foo()
+
+fetch('https://jsonplaceholder.typicode.com/comments')
+.then(response2 => response2.json())
+.then(result => console.log(result))
+
+// response3 = await fetch(
+//     'https://jsonplaceholder.typicode.com/albums',
+//     {
+//         headers:{
+//             'Content-Type':'application/json',
+//         },
+//         method:'GET',
+//         body: JSON.stringify("albums")
+//     });
+//     const result2 = await response3.json();
+//     console.log(result2)
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET",`https://jsonplaceholder.typicode.com/photos`)
+
+xhr.send();
+
+xhr.onload = function (){
+   if (xhr.status == 200){
+    console.log(xhr.status)
+    console.log(xhr.response)
+   }else{
+    alert("ERROR")
+   }
+}
